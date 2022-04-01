f9cfdcb5bb948efdf48afee903ececf07588ce1d
ratuvrnca <2031710119@student.polinema.ac.id>
2022-04-01 09:10:21 +0700

    update

--- lib/dbhelper.dart
+++ lib/dbhelper.dart
@@ -11,7 +11,7 @@ class DbHelper {
   Future<Database> initDb() async {
     //untuk menentukan nama database dan lokasi yg dibuat
     Directory directory = await getApplicationDocumentsDirectory();
-    String path = directory.path + 'waldan.db';
+    String path = directory.path + 'veronica.db';
 
     //create, read databases
     var itemDatabase = openDatabase(path, version: 4, onCreate: _createDb);
